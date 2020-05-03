bcc1ff5b6b3990bb19c429a54411534d32339bbe
HagarMohamedGamal <hagar.mohamed.gamal@gmail.com>
2020-05-03 22:22:07 +0200

    editing conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_03_175220) do
+ActiveRecord::Schema.define(version: 2020_05_03_181423) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
