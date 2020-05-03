a92838555ea34f8edd4cdce641685921ee15905f
SafaaMohamedGamal <safaa.mohamed.gamal@gmail.com>
2020-05-03 22:00:23 +0200

    hanle authentication using username or password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,
-        keys: [:username, :email, :password, :password_confirmation])
+    # handle signup
+    added_attrs = [:username, :email, :password, :password_confirmation]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
